c707a5c359247bf27447a41c047fc80663939b20
Rachaggariani <rachaghariani506@gmail.com>
2022-10-20 19:16:06 +0100

    first commit

---
+++ f1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>les formi=ulaires en php</title>
+<meta charset='utf-8'>
+</head>
+<body>
+<form method="POST" action="targetform.php">
+<p>
+   <label for=prenom">Entre votre prenom</label>
+   <input type="text" name="prenom" id="prenom"/>
+</p>
+<p>
+<input type="submit" value="Envoyer"/>
+</P>
+</form>
+</body>
+</html>
